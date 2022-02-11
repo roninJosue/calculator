3911a14f19cfa218343541ca2c657281b96ab4e4
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-02-10 21:48:40 -0600

    Moving tests files to __tests__ directory

--- src/__tests__/Button.test.js
+++ src/__tests__/Button.test.js
@@ -1,5 +1,5 @@
 import {render, screen} from "@testing-library/react"
-import Button from './Button'
+import Button from "../components/Button"
 
 test('Button render',() => {
   render(<Button />)
--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import {render, screen} from '@testing-library/react'
-import Calculator from "./Calculator"
+import Calculator from "../components/Calculator";
 
 test('Calculator render', async () => {
   render(<Calculator />)
