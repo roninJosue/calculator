8de970649f1d8c976f78e0f625e14810f250ffdd
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-02-11 22:07:17 -0600

    Adding colors to theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,21 @@
 export const lightTheme = {
-  background: '#fff',
-  color: '#00f',
-  buttonBg: '#f00'
+  calculatorBg: '#f3f3f3',
+  color: '#1b1b1b',
+  numberButtonBg: '#ffffff',
+  numberButtonHoverBg: '#fcfcfd',
+  operatorButtonBg: '#f9f9f9',
+  operatorButtonHoverBg: '#faf4f6',
+  equalButtonBg: '#266c88',
+  equalButtonHoverBg: '#3b7993'
 }
 
 export const darkTheme = {
-  background: '#fff',
-  color: '#00f',
-  buttonBg: '#f00'
+  calculatorBg: '#202020',
+  color: '#ffffff',
+  numberButtonBg: '#3b3b3b',
+  numberButtonHoverBg: '#323137',
+  operatorButtonBg: '#323232',
+  operatorButtonHoverBg: '#3f3a3e',
+  equalButtonBg: '#6fc4d5',
+  equalButtonHoverBg: '#67b3c3'
 }
