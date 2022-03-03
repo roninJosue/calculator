f0856151b199bf2328812e2f440d0c4df656a430
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-02 22:07:02 -0600

    Refactor this code later

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,8 +1,11 @@
 // todo if length === 1, and button pressed is 0, do nothing
 export const validateDigit = (expression, digit) => {
   if (isNaN(digit)) {
-    return digit === '.' ? isDot(digit, expression) : (Number(expression) * -1).toString()
+    return digit === '.' ?
+      isDot(digit, expression) :
+      (Number(expression) * -1).toString()
   } else {
+    // TODO check this statement, don't let put numbers like 5.012 or 5.604
     return Number(`${expression}${digit}`).toString()
   }
 }
