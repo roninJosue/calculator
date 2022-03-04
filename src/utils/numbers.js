2f32951c89677b88b8c9359393aa5be36ac9dd24
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-04 16:57:49 -0600

    Remove Leading zeros

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -1,16 +1,18 @@
-// todo if length === 1, and button pressed is 0, do nothing
 export const validateDigit = (expression, digit) => {
   if (isNaN(digit)) {
     return digit === '.' ?
       isDot(digit, expression) :
       (Number(expression) * -1).toString()
   } else {
-    // TODO check this statement, don't let put numbers like 5.012 or 5.604
-    return Number(`${expression}${digit}`).toString()
+    return preventLeadingZeros(`${expression}${digit}`)
   }
 }
 
 const isDot = (dot, expression) => {
   const hasDot = (expression.match(/\./g) || []).length
   return hasDot > 0 ? expression : `${expression}${dot}`
+}
+
+const preventLeadingZeros = (expression) => {
+  return expression.replaceAll(/^0+(?!$)/ig, '')
 }
