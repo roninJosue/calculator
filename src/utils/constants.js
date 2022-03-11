7937a2cdf8aee943379f832d36f68e42e511b214
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-03-10 22:22:04 -0600

    Validating two consecutive operator, if are - permit, else delete one operator.

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,5 @@
 export const NUMBER = 'NUMBER'
 export const OPERATOR = 'OPERATOR'
 export const FUNCTION = 'FUNCTION'
+
+export const operators = ['+','-','*', '/']
--- src/utils/operators.js
+++ src/utils/operators.js
@@ -1,12 +1,24 @@
+import {operators} from "./constants";
+
 export const validateOperator = (formula, operator, evaluated, result) => {
+  //console.log(formula, operator, evaluated, result)
   switch (operator) {
     case '+':
     case '-':
     case '*':
     case '/':
+
+      const lastChar = formula.slice(-1)
+
+      if (operators.includes(lastChar)) {
+        formula = validateFormulaAndOperators(formula, operator, lastChar)
+      } else {
+        formula = `${formula}${operator}`
+      }
+
       return {
         resul: evaluated ? result : operator,
-        formula: evaluated ? `${result}${operator}` : `${formula}${operator}`
+        formula: evaluated ? `${result}${operator}` : `${formula}`
       };
     case '=':
       const res = eval(formula).toString()
@@ -19,4 +31,20 @@ export const validateOperator = (formula, operator, evaluated, result) => {
     default:
       return formula
   }
+}
+
+const validateFormulaAndOperators = (formula, operator, lastChar) => {
+  const minusOp = '-';
+
+  if (lastChar === minusOp && operator === minusOp){
+    const countMinusOp = (formula.match(/-/g) || []).length
+    return countMinusOp > 1 ? formula : `${formula}${operator}`
+  }
+
+  if (operator === minusOp) {
+    return `${formula}${operator}`
+  }
+  const removeLastChar = formula.slice(0, -2)
+
+  return `${removeLastChar}${operator}`
 }
